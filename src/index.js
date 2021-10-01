f9689d8e288c7b9f0b8aa31b706d907870e3e6bb
nadiiazimina <84657846+nadiiazimina@users.noreply.github.com>
2021-10-01 20:49:45 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-class LinkedList {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tall = null;
+  }
+  append(data) {
+    const node = new Node(data);
+
+    if (this.tail) {
+      this.tail.next = node;
+    }
+
+    if (!this.head) {
+      this.head = node;
+    }
+    this.tail = node;
+  }
+
+  prepend(data) {
+    const node = new Node(data, this.head);
+
+    this.head = node;
+
+    if (!this.tail) {
+      this.tail = node;
+    }
+  }
+
+  toArray() {
+    let current = this.head;
+
+    while (current) {
+      const output = [];
+      let current = this.head;
+
+      while (current) {
+        output.push(current);
+        current = current.next;
+      }
+
+      return output;
+    }
+  }
+}
+
+const list = new LinkedList();
+list.append("My");
+list.append("name");
+list.prepend("Hi");
+
+console.log(list.toArray());
